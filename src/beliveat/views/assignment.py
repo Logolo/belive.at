c46b9b6e7ccec8b9bdbee3a2a496e0e55c52fffd
James Arthur <thruflo@gmail.com>
2012-04-12 15:47:28 +0100

    Create assignment view.

--- src/beliveat/views/assignment.py
+++ src/beliveat/views/assignment.py
@@ -7,20 +7,36 @@ logger = logging.getLogger(__name__)
 
 from pyramid.view import view_config
 
-from ..model import Assignment
+from pyramid_simpleform import Form
+from pyramid_simpleform.renderers import FormRenderer
 
-@view_config(route_name='assignments', name='create', renderer='json')
-def create_assignment_view(request):
-    """"""
+from ..model import Assignment, Hashtag, save as save_to_db
+from ..schema import CreateAssignment
+
+@view_config(route_name='assignments', request_method='GET', xhr=True,
+        renderer='json')
+def get_assignments_view(request):
+    """Get assignments sorted by rating."""
     
-    # schema.CreateAssignment
+    raise NotImplementedError
+
+
+@view_config(route_name='assignments', name='create', request_method='POST',
+        xhr=True, renderer='json')
+def create_assignment_view(request, form_cls=Form):
+    """Create an assignment."""
     
-    # validate user input
-    # create the Assignment
-    
-    # create promote offer (n.b.: remember if statement so don't show content
-    # to it's originator to RT and auth increment the count if it's your own
-    # content)
-    
-    return {} # assignment.__json__()
+    form = form_cls(request, schema=CreateAssignment)
+    if form.validate():
+        d = form.data
+        assignment = Assignment(title=d['title'], description=d['description'])
+        assignment.hashtag = Hashtag.get_or_create(d['hashtag'])
+        assignment.author = request.user
+        save_to_db(assignment)
+        # XXX create promote offer (n.b.: remember if statement so don't show
+        # content to it's originator to RT and auth increment the count if
+        # it's your own content)
+        return assignment.__json__()
+    request.response.status_int = 400
+    return form.errors
 
