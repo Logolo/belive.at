80924800cf50c2114c122d43d1ca9e9d45af879c
James Arthur <thruflo@gmail.com>
2012-05-14 18:32:30 +0100

    refactor: simplified schemas in line with traversal machinery.
    Plus renamed to use `Create...` consistently.

--- src/beliveat/schema.py
+++ src/beliveat/schema.py
@@ -51,27 +51,12 @@ class CreateAssignment(FlexibleSchema):
     """Form fields to validate when creating assignment."""
     
     title = validators.UnicodeString(max=140, not_empty=True)
-    hashtag = Hashtag(max=32, not_empty=True)
     description = validators.UnicodeString()
-    
 
-class MakeOffer(FlexibleSchema):
+class CreateOffer(FlexibleSchema):
     """Form fields to validate when making an offer to cover or promote an
       assignment.
     """
     
-    # The assignment's public id.
-    assignment = validators.Int(not_empty=True)
-    hashtag = Hashtag(max=32, not_empty=True)
     note = validators.UnicodeString(max=140, if_missing=u'')
-
-class CloseOffer(FlexibleSchema):
-    """Form fields to validate when closing an offer."""
-    
-    # The offer's public id.
-    id = validators.Int(not_empty=True)
-    # The assignment's public id.
-    assignment = validators.Int(not_empty=True)
-    # What type of offer is it?
-    offer_type = validators.OneOf(['cover', 'promote'])
 
