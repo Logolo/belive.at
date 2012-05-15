5d338322cf179eead9b90f7e9adebf7b6ccbf66b
James Arthur <thruflo@gmail.com>
2012-05-15 18:11:36 +0100

    events: added ORM events for tweets and records.
    `TweetAdded`, `CoverageRecordAdded` and `PromotionRecordAdded` ORM events.

--- src/beliveat/events.py
+++ src/beliveat/events.py
@@ -26,6 +26,25 @@ class IStoryDeleted(Interface):
     story = Attribute('Story instance.')
 
 
+class ITweetAdded(Interface):
+    """An event type that is emitted when a tweet is added."""
+    
+    request = Attribute('Request object.')
+    tweet = Attribute('Tweet instance.')
+
+class ICoverageRecordAdded(Interface):
+    """An event type that is emitted when a coverage record is added."""
+    
+    request = Attribute('Request object.')
+    record = Attribute('CoverageRecord instance.')
+
+class IPromotionRecordAdded(Interface):
+    """An event type that is emitted when a promotion record is added."""
+    
+    request = Attribute('Request object.')
+    record = Attribute('PromotionRecord instance.')
+
+
 @implementer(IStoryAdded)
 class StoryAdded(object):
     """An instance of this class is emitted whenever a story is added."""
@@ -53,6 +72,37 @@ class StoryDeleted(object):
     def __init__(self, request, story, data=None):
         self.request = request
         self.story = story
+        self.data = data
+    
+
+
+@implementer(ITweetAdded)
+class TweetAdded(object):
+    """An instance of this class is emitted whenever a tweet is added."""
+    
+    def __init__(self, request, tweet, data=None):
+        self.request = request
+        self.tweet = tweet
+        self.data = data
+    
+
+@implementer(ICoverageRecordAdded)
+class CoverageRecordAdded(object):
+    """An instance of this class is emitted when a coverage record is added."""
+    
+    def __init__(self, request, record, data=None):
+        self.request = request
+        self.record = record
+        self.data = data
+    
+
+@implementer(IPromotionRecordAdded)
+class PromotionRecordAdded(object):
+    """An instance of this class is emitted when a promotion record is added."""
+    
+    def __init__(self, request, record, data=None):
+        self.request = request
+        self.record = record
         self.data = data
     
 
